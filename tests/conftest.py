ce0adb55f99019e61e6f9bb0d37b9d254002a9c7
ZanSara <sarazanzo94@gmail.com>
2022-04-17 22:01:32 +0200

    Fix path to app in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,47 +1,6 @@
 from fastapi.testclient import TestClient
 
-from flashcards_server.main import app  # oauth2_scheme
+from flashcards_server.app import app
 
 
 client = TestClient(app)
-
-
-# class MockOAuth(OAuth2):
-#     def __init__(
-#         self,
-#         tokenUrl: str,
-#         scheme_name: Optional[str] = None,
-#         scopes: Optional[Dict[str, str]] = None,
-#         description: Optional[str] = None,
-#         auto_error: bool = True,
-#     ):
-#         if not scopes:
-#             scopes = {}
-#         flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
-#         super().__init__(
-#             flows=flows,
-#             scheme_name=scheme_name,
-#             description=description,
-#             auto_error=auto_error,
-#         )
-
-#     async def __call__(self, request: Request) -> Optional[str]:
-#         authorization: str = request.headers.get("Authorization")
-#         scheme, param = get_authorization_scheme_param(authorization)
-#         if not authorization or scheme.lower() != "bearer":
-#             if self.auto_error:
-#                 raise HTTPException(
-#                     status_code=HTTP_401_UNAUTHORIZED,
-#                     detail="Not authenticated",
-#                     headers={"WWW-Authenticate": "Bearer"},
-#                 )
-#             else:
-#                 return None
-#         return param
-
-
-# async def mock_auth(tokenUrl: Optional[str] = "token"):
-#     return MockOAuth()
-
-
-# app.dependency_overrides[oauth2_scheme] = override_dependency
